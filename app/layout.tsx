0365d4aedb3384bf2890358a39f59f6557febeba
Justin West <justin.alex.west@gmail.com>
2023-01-05 19:58:37 -0800

    A Start

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import { Html } from "next/document";
 import "./globals.css";
+import Link from "next/link";
 
 export default function RootLayout({
   children,
@@ -11,8 +11,24 @@ export default function RootLayout({
       <head></head>
 
       <body className="bg-slate-900">
-        <nav className="bg-slate-700 text-slate-200 h-10 rounded-lg m-4 p-2 uppercase">
-          This is a nav bar. It is always there |XD
+        <nav className="bg-slate-700 text-slate-200  h-10 rounded-lg m-4 p-2 navbar">
+          <div className="inline-block text-xl font-extrabold mr-4 my-auto">
+            Alexandria.
+          </div>
+          <div className="inline-block align-top">
+            <Link
+              className="bg-black p-1 px-6 rounded-full hover:bg-slate-500 transition-colors duration-200 text-base font-normal mr-4 my-auto"
+              href="/"
+            >
+              Home
+            </Link>{" "}
+            <Link
+              className="bg-black p-1 px-6 rounded-full hover:bg-slate-500 transition-colors duration-200 text-base font-normal mr-4 my-auto"
+              href="/dash"
+            >
+              Dashboard
+            </Link>
+          </div>
         </nav>
         {children}
       </body>
