9199af3ad54cea5b687131a05d0778ba430636cc
Justin West <39067251+abluewizard@users.noreply.github.com>
2023-01-06 16:24:05 -0800

    Refining the basics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "./globals.css";
 import Navbar from "./navbar";
+import { Poppins, Old_Standard_TT } from "@next/font/google";
+const poppinsMD = Poppins({
+  weight: "300",
+  subsets: ["latin"],
+});
+
+const olstdMD = Old_Standard_TT({
+  weight: "400",
+  subsets: ["latin"],
+});
 export default function RootLayout({
   children,
 }: {
@@ -8,8 +18,8 @@ export default function RootLayout({
   return (
     <html>
       <head></head>
-      <body className="bg-black">
-        <Navbar />
+      <body className={poppinsMD.className}>
+        <Navbar titlefont={olstdMD} />
         {children}
       </body>
     </html>
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,8 +1,13 @@
 "use client";
+import { Old_Standard_TT } from "@next/font/google";
 import Link from "next/link";
 import useMousePosition from "./mouspos";
+const olstdMD = Old_Standard_TT({
+  weight: "400",
+  subsets: ["latin"],
+});
 
-export default function Navbar() {
+export default function Navbar({ titlefont }) {
   const mousepos = useMousePosition();
 
   let root = document.documentElement;
@@ -19,23 +24,34 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="navbar nav-glow grid text-white h-16  m-4  bg-amber-900 p-[1px] items-stretch ">
-      <div className=" Nav-Content flex bg-black items-stretch gap-4 px-4 z-[2]">
-        <div className="inline-block text-xl font-extrabold my-auto">
+    <nav className="navbar nav-glow grid text-white h-16  mt-4  bg-amber-900 py-[1px] items-stretch ">
+      <div className=" Nav-Content flex  items-stretch px-4 z-[2] backdrop-blur-md backdrop-brightness-[0.1]">
+        <Link
+          className={
+            "inline-block text-3xl my-auto mr-auto " + titlefont.className
+          }
+          href="/"
+        >
           Justin West
-        </div>
-        <div className="flex">
+        </Link>
+        <div className="flex ">
           <Link
-            className="nav-glow inline-block bg-amber-800 p-1 px-6 rounded-md   transition-colors duration-200 text-base font-normal mr-4 my-auto"
-            href="/"
-          >
-            Home
-          </Link>
-          <Link
-            className="nav-glow inline-block bg-amber-800 p-1 px-6 rounded-md transition-colors duration-200 text-base font-normal mr-4 my-auto"
+            className="nav-glow inline-block bg-[hsl(23,86%,8%)] text-center w-32 p-1 px-6 rounded-md transition-colors duration-200 mr-4 my-auto"
             href="/dash"
           >
-            About
+            Resume
+          </Link>
+          <Link
+            className="nav-glow inline-block bg-[hsl(23,86%,8%)] text-center w-32  p-1 px-6 rounded-md transition-colors duration-200 mr-4 my-auto"
+            href="/dash"
+          >
+            Projects
+          </Link>
+          <Link
+            className="nav-glow inline-block bg-[hsl(23,86%,8%)]  text-center w-32 p-1 px-6 rounded-md transition-colors duration-200 mr-4 my-auto"
+            href="/dash"
+          >
+            About Me
           </Link>
         </div>
       </div>
