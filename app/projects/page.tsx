63058b5e07eba434b91cbbf0ba8ac5b9f515dead
Justin West <justin.alex.west@gmail.com>
2023-01-08 15:56:11 -0800

    adding projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,29 @@
+import Link from "next/link";
+
 export default function Projects() {
-  return <>This is where i post my projects.</>;
+  return (
+    <>
+      <div className="flex px-8">
+        <Link
+          href="/projects/technology"
+          className="project-card grid w-96 h-96"
+        >
+          <span className=" nav-glow nav-glow2 bg-amber-950 h-96 rounded-xl">
+            ?
+          </span>
+          <span className="h-min text-center">Technology</span>
+        </Link>
+
+        <Link
+          href="/projects/technology"
+          className="project-card grid w-96 h-96"
+        >
+          <span className="nav-glow nav-glow2 bg-amber-950 h-96 rounded-xl">
+            ?
+          </span>
+          <span className="h-min text-center">Design</span>
+        </Link>
+      </div>
+    </>
+  );
 }
