f8ee52f76667fc81c468074c8047ea22008ea249
Justin West <justin.alex.west@gmail.com>
2023-03-29 06:54:22 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "export",
-  images: { unoptimized: true },
   experimental: {
     appDir: true,
   },
