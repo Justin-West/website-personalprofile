f8b1a04be3ff73edaceb4d6d167689ddb1a5932a
Justin West <justin.alex.west@gmail.com>
2023-04-10 11:59:59 -0700

    Update page.tsx

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,7 +5,7 @@ export default function Resume() {
     <main className=" w-full p-4 overflow-hidden ">
       <iframe
         className="gframe border border-amber-900 mx-auto rounded-md w-full p-0"
-        src="https://drive.google.com/file/d/1nNHkc9T7DzNLiWIlkNZw8X5UAKdZFbo2/preview"
+        src="https://drive.google.com/file/d/1O-13vsqeHLzrDfaqql_hJYoeo3f36zoj/preview"
         width="100%"
         height="900px"
         allow="autoplay"
